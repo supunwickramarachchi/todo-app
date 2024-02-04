e980b77bcac5dfed07077ec1a1e8612214902b50
SpaceSeeker101 <86275338+SpaceSeeker101@users.noreply.github.com>
2024-02-04 20:48:15 +0530

    Implemented the Complete and Exit buttons

--- gui.py
+++ gui.py
@@ -7,9 +7,14 @@ add_button = PySimpleGUI.Button("Add")
 list_box = PySimpleGUI.Listbox(values=functions.get_todos(), key='todos',
                                enable_events=True, size=[45, 10])
 edit_button = PySimpleGUI.Button("Edit")
+complete_button = PySimpleGUI.Button("Complete")
+exit_button = PySimpleGUI.Button("Exit")
 
 window = PySimpleGUI.Window("To-Do App",
-                            layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                            layout=[[label],
+                                    [input_box, add_button],
+                                    [list_box, edit_button, complete_button],
+                                    [exit_button]],
                             font=('Helvetica', 15))
 while True:
     event, values = window.read()
@@ -34,8 +39,20 @@ while True:
             functions.write_todos(todos)
             window['todos'].update(values=todos)
 
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+
+        case "Exit":
+            break
+
         case 'todos':
             window['todo'].update(value=values['todos'][0])
+
         case PySimpleGUI.WIN_CLOSED:
             break
 
